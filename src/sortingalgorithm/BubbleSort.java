449ec7c05d46670c9fee630d11db76466bfa092e
wangsiyu12344 <wangsiyu12344@163.com>
2020-08-24 19:57:34 +0900

    排序算法

--- src/sortingalgorithm/BubbleSort.java
+++ src/sortingalgorithm/BubbleSort.java
@@ -1,18 +1,21 @@
 package sortingalgorithm;
 
 /**
- * 冒泡排序
+ * 冒泡排序， 每一轮排序都依次比较相邻两个位置的数据，把大的放在后面。
+ * 时间复杂度O(n^2), 空间复杂度O(1)。
  */
 public class BubbleSort {
-    public int [] bubbleSort(int arr[]) {
+    public int[] bubbleSort(int arr[]) {
+        if(arr == null || arr.length == 0) return arr;
+
         int len = arr.length;
 
-        for (int i = 0; i < len-1; i++) {
-            for (int j = 0; j < len-1-i; j++) {
-                if(arr[j-1] > arr[j]){
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = 0; j < len - 1 - i; j++) {
+                if (arr[j - 1] > arr[j]) {
                     int temp = arr[j];
-                    arr[j] = arr[j-1];
-                    arr[j-1] = temp;
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
                 }
             }
         }
